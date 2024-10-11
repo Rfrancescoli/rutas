0cdfc01b31cab4d5388a17a9c59e2f0363c9baba
Rfrancescoli <vicharramarcosgereda@gmail.com>
2024-10-11 12:31:52 -0500

    Interfaz registro de clientes

--- views/clientes/listar-clientes.php
+++ views/clientes/listar-clientes.php
@@ -8,14 +8,52 @@ require_once '../../app/config/App.php';
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
-  <?php echo renderContentHeader("Clientes", "Inicio") ?>
+  <?php echo renderContentHeader("Clientes", "Inicio", SERVERURL . "views") ?>
   <!-- /.content-header -->
 
   <!-- Main content -->
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <!-- Contenido personalizado -->
+        <div class="col-md-12">
+
+          <a href="./registrar-clientes" class="btn btn-primary btn-sm">Registrar</a>
+          <hr>
+
+          <div class="table-responsive">
+            <table class="table table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Apellidos</th>
+                  <th>Nombres</th>
+                  <th>Teléfono</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Marcos Gereda</td>
+                  <td>Rubén Francescoli</td>
+                  <td>981369757</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Marcos Gereda</td>
+                  <td>Rubén Francescoli</td>
+                  <td>981369757</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Marcos Gereda</td>
+                  <td>Rubén Francescoli</td>
+                  <td>981369757</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+
       </div>
       <!-- /.row -->
     </div><!-- /.container-fluid -->
